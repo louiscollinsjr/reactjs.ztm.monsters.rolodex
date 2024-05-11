04b4f0baff1b371254211d27a1959d9168f7fa13
louiscollinsjr <louiscollinsjr@gmail.com>
2024-05-11 19:46:44 +0300

    desctructoring and removed annon function

--- src/App.js
+++ src/App.js
@@ -29,13 +29,24 @@ class App extends Component {
     console.log('componentDidMount');
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    console.log(searchField);
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
     console.log('render');
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
-    });
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this;
 
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
 
     return (
       <div className="App">
@@ -43,18 +54,9 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="search monsters"
-          onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase();
-            console.log(searchField);
-           
-
-            this.setState(() => {
-              return {searchField};
-            });
-
-          }}
+          onChange={onSearchChange}
         />
-        
+
         {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
